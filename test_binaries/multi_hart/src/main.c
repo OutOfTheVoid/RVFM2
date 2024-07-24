f0143241f1f7753977941ca5eabac9900abdaefd
liam <liam@augmend.com>
2024-07-24 12:07:10 -0700

    remove test_binaries directory

--- test_binaries/multi_hart/src/main.c
+++
@@ -1,9 +0,0 @@
-#include "dbg.h"
-#include "intrin.h"
-
-void main() {
-    debug_print("Hello, world!");
-    while (1) {
-        wfi();
-    }
-}
--- test_binaries/rust/target/riscv32i-unknown-none-elf/release/deps/main-60d5f247b934a028.d
+++
@@ -1,6 +0,0 @@
-/Users/liam/rvfm2/test_binaries/rust/target/riscv32i-unknown-none-elf/release/deps/main-60d5f247b934a028: src/main.rs src/init.s
-
-/Users/liam/rvfm2/test_binaries/rust/target/riscv32i-unknown-none-elf/release/deps/main-60d5f247b934a028.d: src/main.rs src/init.s
-
-src/main.rs:
-src/init.s:
